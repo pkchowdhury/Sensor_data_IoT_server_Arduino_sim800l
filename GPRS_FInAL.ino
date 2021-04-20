bb76d6a9ed107579eed9196e05907f61eb722828
Pallab Chowdhury <chowdhurypall95@gmail.com>
2021-04-20 13:45:38 +0600

    Add files via upload

---
+++ GPRS_FInAL.ino
@@ -0,0 +1,107 @@
+#include <SoftwareSerial.h>
+SoftwareSerial gprsSerial(3,2);
+ 
+#include <String.h>
+ int lm35Pin = 0;
+void setup()
+{
+  gprsSerial.begin(9600);               // the GPRS baud rate   
+  Serial.begin(9600);    // the GPRS baud rate 
+  //dht.begin();
+ 
+  delay(1000);
+}
+ 
+void loop()
+{
+  int val = 0;
+  val=analogRead(lm35Pin);
+  float mv=(val/1023.0)*5000;
+  float cel= (mv/10); 
+      
+    // convert to string
+  char buf[16];
+  String strTemp = dtostrf(cel, 4, 1, buf);
+  
+  Serial.println(strTemp);    
+      
+   
+  if (gprsSerial.available())
+    Serial.write(gprsSerial.read());
+ 
+  gprsSerial.println("AT");
+  delay(1000);
+ 
+  gprsSerial.println("AT+CPIN?");
+  delay(1000);
+ 
+  gprsSerial.println("AT+CREG?");
+  delay(1000);
+ 
+  gprsSerial.println("AT+CGATT?");
+  delay(1000);
+ 
+  gprsSerial.println("AT+CIPSHUT");
+  delay(1000);
+ 
+  gprsSerial.println("AT+CIPSTATUS");
+  delay(2000);
+ 
+  gprsSerial.println("AT+CIPMUX=0");
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CSTT=\"gpinternet\"");//start task and setting the APN,
+  delay(1000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIICR");//bring up wireless connection
+  delay(3000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIFSR");//get local IP adress
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIPSPRT=0");
+  delay(3000);
+ 
+  ShowSerialData();
+  
+  gprsSerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");//start up the connection
+  delay(6000);
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIPSEND");//begin send data to remote server
+  delay(4000);
+  ShowSerialData();
+  
+  String str="GET https://api.thingspeak.com/update?api_key="API KEY"&field1=" + String(strTemp); //put your thingspeak write api key here
+  Serial.println(str);
+  gprsSerial.println(str);//begin send data to remote server
+  
+  delay(4000);
+  ShowSerialData();
+ 
+  gprsSerial.println((char)26);//sending
+  delay(5000);//waitting for reply, important! the time is base on the condition of internet 
+  gprsSerial.println();
+ 
+  ShowSerialData();
+ 
+  gprsSerial.println("AT+CIPSHUT");//close the connection
+  delay(100);
+  ShowSerialData();
+} 
+void ShowSerialData()
+{
+  while(gprsSerial.available()!=0)
+  Serial.write(gprsSerial.read());
+  delay(5000); 
+  
+}
